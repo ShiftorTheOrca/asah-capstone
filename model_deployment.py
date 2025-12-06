a5cf02aba62b86a1bacc98cd876a89f45dd54264
Nelson <ahlinelson04@gmail.com>
2025-12-06 23:12:23 +0700

    add line chart sales per bulan, pairplot rfm, pie chart sales contribution per cluster

--- model_deployment.py
+++ model_deployment.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder
 from threading import RLock
+import plotly.graph_objects as go
+
 
 _lock = RLock()
+sns.set_style("whitegrid")
 
 @st.cache_data
 def load_data():
@@ -53,7 +56,7 @@ def plot_categorical_columns(df):
         for i, column in enumerate(df[categorical_cols].columns):
             top_values = df[column].value_counts().head(12)
             
-            sns.barplot(x=top_values.values, y=top_values.index, ax=axes[i], palette='plasma', legend=False)
+            sns.barplot(x=top_values.values, y=top_values.index, ax=axes[i], palette='plasma', hue=top_values.index, legend=False)
             axes[i].set_title(column, fontsize=13, weight='bold')
             axes[i].set_xlabel('Count', fontsize=13)
             
@@ -156,28 +159,203 @@ def plot_most_spend_seasonality_per_cluster(df):
         
     return fig
 
-# Streamlit App
-st.set_page_config(layout="centered")
-st.title("Online Retail UCI Clustering Data")
+@st.cache_data
+def calculate_rfm(df):
+    df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+    analysis_date = df['InvoiceDate'].max() + pd.Timedelta(days=1)
 
+    Recency = ('InvoiceDate', lambda x: (analysis_date - x.max()).days)
+    Frequency = ('InvoiceNo', 'nunique')
+    Monetary = ('Sales', 'sum')
+    Cluster = ('Cluster', 'first')
+    Label = ('Label', 'first')
+
+    df_rfm = df.groupby('CustomerID').agg(
+        Recency=Recency,
+        Frequency=Frequency,
+        Monetary=Monetary,
+        Cluster=Cluster,
+        Label=Label
+    ).reset_index()
+        
+    return df_rfm
+
+@st.cache_data
+def plot_3d_rfm(df_rfm):
+    visual_3d = go.Scatter3d(
+        x= df_rfm['Recency'],
+        y= df_rfm['Frequency'],
+        z= df_rfm['Monetary'],
+        hovertemplate=(
+            "Recency: %{x}<br>"
+            "Frequency: %{y}<br>"
+            "Monetary: %{z:.2f}"),
+        mode='markers',
+        marker=dict(
+            color = df_rfm['Cluster'],
+            size= 12,
+            line=dict(
+                color= df_rfm['Cluster'],
+                width= 6
+            ),
+            opacity=0.8
+        )
+    )
+
+    data = [visual_3d]
+    layout = go.Layout(
+        title= 'Visualisasi 3D',
+        scene = dict(
+                xaxis = dict(title  = 'Recency'),
+                yaxis = dict(title  = 'Frequency'),
+                zaxis = dict(title  = 'Monetary')
+            )
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+        
+    return fig
+
+@st.cache_data
+def analyse_cluster(df_rfm):
+    cluster_analysis = df_rfm.groupby('Cluster').agg(
+        Avg_Recency=('Recency', 'mean'),
+        Avg_Frequency=('Frequency', 'mean'),
+        Avg_Monetary=('Monetary', 'mean'),
+        Count=('CustomerID', 'count'),
+        Percentage=('CustomerID', lambda x: (x.count() / len(df_rfm)) * 100),
+        Label=('Label', 'first')
+    ).reset_index().sort_values(by='Cluster')
+    
+    return cluster_analysis
+
+@st.cache_data
+def plot_monthly_sales_per_cluster(df):
+    df_months = df.copy()
+    df_months['Months'] = pd.to_datetime(df_months['InvoiceDate']).dt.month
+    monthly_sales_cluster = df_months.groupby(['Cluster', 'Months'])['Sales'].sum().reset_index()
+
+    fig, ax = plt.subplots(1, 1, figsize=(12, 6))
+
+    sns.lineplot(x='Months', y='Sales', data=monthly_sales_cluster, palette='plasma', hue='Cluster', ax=ax)
+    ax.set_title('Monthly Sales per Cluster', fontsize=13, weight='bold')
+    ax.set_xticks(range(1, 13))
+
+    fig.tight_layout()
+    
+    return fig
+
+@st.cache_data
+def plot_segmentation_and_monetary_per_cluster(df):
+    cluster_analysis = analyse_cluster(df)
+    values = []
+    labels = []
+
+    values.append(cluster_analysis['Percentage'])
+    labels.append(cluster_analysis['Label'])
+    values.append(cluster_analysis['Avg_Monetary'])
+    labels.append(cluster_analysis['Label'])
+
+    fig, axes = plt.subplots(1, 2, figsize=(12, 12))
+    axes = axes.flatten()
+
+    axes[0].pie(x=values[0], labels=labels[0], autopct='%1.1f%%', colors=sns.color_palette('pastel', len(labels[0])))
+    axes[0].set_title('Customer Segmentation by Labels', fontsize=13, weight='bold')
+    axes[1].pie(x=values[1], labels=labels[1], autopct='%1.1f%%', colors=sns.color_palette('pastel', len(labels[1])))
+    axes[1].set_title('Sales Contribution per Labels', fontsize=13, weight='bold')
+
+    fig.tight_layout()
+    
+    return fig
+
+@st.cache_data
+def plot_pairplot_rfm(df_rfm):
+    columns = ['Recency', 'Frequency', 'Monetary']
+    
+    pairplot = sns.pairplot(data=df_rfm, vars=columns, hue='Cluster', palette='plasma')
+    fig = pairplot.figure
+    fig.suptitle('Pair Plot RFM per Cluster', fontsize=13, weight='bold')
+
+    fig.tight_layout()
+    
+    return fig
+
+# Streamlit Deployment
+st.set_page_config(layout="wide", page_title="Peningkatan Efisiensi Strategi Pemasaran Berbasis Visualisasi Segmen Pelanggan")
+
+if 'page' not in st.session_state:
+    st.session_state.page = "home"
+    
 df = load_data()
-st.dataframe(df)
+df_copy = df.copy()
 
-# Menampilkan Bar Chart kolom numeric
-fig = plot_numerical_columns(df)
-st.pyplot(fig=fig, clear_figure=True, width='stretch')
+with st.sidebar:
+    st.write("## Menu")
+    if st.button("Home", width='stretch'):
+        st.session_state.page = "home"
+    if st.button("Data Analysis", width='stretch'):
+        st.session_state.page = "data_analysis"
+    if st.button("RFM Analysis", width='stretch'):
+        st.session_state.page = "rfm_analysis"
 
-# Menampilkan Bar Chart kolom kategorikal
-fig = plot_categorical_columns(df)
-st.pyplot(fig=fig, clear_figure=True, width='stretch')
-# Menampilkan Heatmap korelasi antar data
-fig = plot_correlation_heatmap(df)
-st.pyplot(fig=fig, clear_figure=True, width='stretch')
+if st.session_state.page == "home":
+    st.title("Peningkatan Efisiensi Strategi Pemasaran Berbasis Visualisasi Segmen Pelanggan", text_alignment='center')
+    st.write("""
+    This application performs customer segmentation using clustering techniques on the Online Retail UCI dataset.
+    Navigate through the sidebar to explore data analysis and RFM analysis of customer segments.
+    """)
+if st.session_state.page == "data_analysis":
+    st.title("Online Retail UCI Clustering Data")
+    
+    cluster_filter = st.selectbox("Filter Cluster", options=['All', 'Loyal Customer', 'Regular Customer', 'Churn/One Time Spender', 'High Risk Churn'])
+    
+    if cluster_filter != 'All':
+        df_copy = df_copy[df_copy['Label'] == cluster_filter]
+        
+    st.dataframe(df_copy)
+    st.download_button(label="Download CSV", data=df.to_csv(index=False), file_name='online_retail_uci_clustering.csv')
 
-# Menampilkan Pie Chart rasio Category tertinggi per Cluster
-fig = plot_most_spend_category_per_cluster(df)
-st.pyplot(fig=fig, clear_figure=True, width='stretch')
+    # Menampilkan Bar Chart kolom numeric
+    fig = plot_numerical_columns(df_copy)
+    st.pyplot(fig=fig, clear_figure=True, width='stretch')
 
-# Menampilkan Pie Chart rasio Seasonality tertinggi per Cluster
-fig = plot_most_spend_seasonality_per_cluster(df)
-st.pyplot(fig=fig, clear_figure=True, width='stretch')
+    # Menampilkan Bar Chart kolom kategorikal
+    fig = plot_categorical_columns(df_copy)
+    st.pyplot(fig=fig, clear_figure=True, width='stretch')
+    # Menampilkan Heatmap korelasi antar data
+    fig = plot_correlation_heatmap(df_copy)
+    st.pyplot(fig=fig, clear_figure=True, width='stretch')
+
+    # Menampilkan Pie Chart rasio Category tertinggi per Cluster
+    fig = plot_most_spend_category_per_cluster(df_copy)
+    st.pyplot(fig=fig, clear_figure=True, width='stretch')
+
+    # Menampilkan Pie Chart rasio Seasonality tertinggi per Cluster
+    fig = plot_most_spend_seasonality_per_cluster(df_copy)
+    st.pyplot(fig=fig, clear_figure=True, width='stretch')
+    
+    fig = plot_monthly_sales_per_cluster(df_copy)
+    st.pyplot(fig=fig, clear_figure=True, width='stretch')
+    
+elif st.session_state.page == "rfm_analysis":
+    df_rfm = calculate_rfm(df_copy)
+    
+    st.title("RFM Analysis")
+
+    cluster_filter_rfm = st.selectbox("Filter Cluster", options=['All', 'Loyal Customer', 'Regular Customer', 'Churn/One Time Spender', 'High Risk Churn'])
+    if cluster_filter_rfm != 'All':
+        df_rfm = df_rfm[df_rfm['Label'] == cluster_filter_rfm]
+        
+    st.dataframe(df_rfm)
+
+    # Menampilkan visualisasi 3D dari hasil Clustering
+    fig = plot_3d_rfm(df_rfm)
+    st.plotly_chart(figure_or_data=fig, height=900)
+
+    # Menampilkan Pie Chart segmentasi Customer per Cluster
+    fig = plot_segmentation_and_monetary_per_cluster(df_rfm)
+    st.pyplot(fig=fig, clear_figure=True, width='stretch')
+    
+    # Menampilkan Pairplot RFM per Cluster
+    fig = plot_pairplot_rfm(df_rfm)
+    st.pyplot(fig=fig, clear_figure=True, width='stretch')
