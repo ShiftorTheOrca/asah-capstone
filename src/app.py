cca37a44c6bf714b25a93c9da56ff3319d11499a
Nelson <ahlinelson04@gmail.com>
2025-12-10 11:51:36 +0700

    integrated sistem rekomendasi to website

--- src/app.py
+++ src/app.py
@@ -1,7 +1,11 @@
 # Import Library
+import time
 import pandas as pd
+import numpy as np
 
 from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -284,6 +288,95 @@ def plot_pairplot_rfm(df_rfm):
     
     return fig
 
+@st.cache_data
+def recommendation_system(df):
+    items = df[['StockCode', 'Description', 'Category']]
+    items = items.drop_duplicates().reset_index().drop('index', axis=1)
+    items['Description'] = items['Description'].apply(lambda desc: desc.strip())
+    items['Tags'] = items['Description'] + " " + items['Category']
+    items['Tags'] = items['Tags'].apply(lambda x:x.lower().strip())
+
+    cv = CountVectorizer(stop_words='english')
+    vector = cv.fit_transform(items['Tags']).toarray()
+    similarity = cosine_similarity(vector)
+    
+    sales_by_stockcode = df.groupby(['CustomerID', 'StockCode'])['Sales'].sum()
+    favorite_items = sales_by_stockcode.groupby('CustomerID').nlargest(5).droplevel(level=1).reset_index()
+
+    customer_dict = favorite_items.groupby('CustomerID')['StockCode'].apply(list).to_dict()
+    
+    return items, similarity, customer_dict
+
+@st.cache_data
+def recommend_item_description(item_name_full, top_n=5):
+    data = ""
+    index = items[items['Description'] == item_name_full].index[0]
+    distances = sorted(list(enumerate(similarity[index])),reverse=True,key = lambda x: x[1])
+    
+    data = f"Karena Anda membeli {items.iloc[index]['Description']}, mungkin Anda tertarik: "
+    
+    for i in distances[1:top_n+1]:
+        data += f"\n- {items.iloc[i[0]].Description} (Similarity: {i[1]:.4f})"
+        
+    return data
+        
+@st.cache_data
+def recommend_for_single_customer(id_customer, top_n=5):
+    data = ""
+    
+    if id_customer not in customer_dict:
+        data += f"CustomerID {id_customer} tidak ditemukan"
+        return data
+
+    data += f"CustomerID: {id_customer}"
+
+    stock_codes = customer_dict[id_customer]
+    all_similarities = []
+
+    for stock_code in stock_codes:
+        index = items[items['StockCode'] == stock_code].index[0]
+        distances = sorted(list(enumerate(similarity[index])))
+        all_similarities.extend(distances)
+
+        data += f"\n- Anda membeli {items.iloc[index]['Description']}"
+
+    similarity_dict = {}
+
+    for idx, sim in all_similarities:
+        if idx in similarity_dict:
+            similarity_dict[idx].append(sim)
+        else:
+            similarity_dict[idx] = [sim]
+
+    avg_similarities = []
+
+    for idx, sims in similarity_dict.items():
+        if items.iloc[idx]['StockCode'] not in stock_codes:
+            avg_similarities.append((idx, np.mean(sims)))
+
+    avg_similarities = sorted(avg_similarities,reverse=True, key = lambda x: x[1])
+    recommendations = []
+
+    for i in range(top_n):
+        idx, sim_score = avg_similarities[i]
+        recommendations.append({
+            'StockCode': items.iloc[idx]['StockCode'],
+            'Description': items.iloc[idx]['Description'],
+            'Similarity': sim_score
+        })
+
+    data += "\n\nRekomendasi untuk customer ini:"
+
+    for i, rec in enumerate(recommendations):
+        data += f"\n{i + 1}. {rec['Description']} (Similarity: {rec['Similarity']:.4f})"
+        
+    return data
+
+def stream_data(data):
+    for word in data.split(" "):
+        yield word + " "
+        time.sleep(0.02)
+
 # Streamlit Deployment App
 df = load_data()
 df_copy = df.copy()
@@ -313,6 +406,9 @@ with st.sidebar:
         
     if st.button("RFM Analysis", width='stretch'):
         st.session_state.page = "rfm_analysis"
+        
+    if st.button("Recommendation System", width='stretch'):
+        st.session_state.page = "recommendation_system"
 
 if st.session_state.page == "home":
     st.title("Peningkatan Efisiensi Strategi Pemasaran Berbasis Visualisasi Segmen Pelanggan", text_alignment='center')
@@ -514,3 +610,75 @@ elif st.session_state.page == "rfm_analysis":
                 - Dari visualisasi ini, kita dapat mengamati pola distribusi pelanggan dalam setiap cluster berdasarkan metrik RFM mereka. Misalnya, pelanggan dalam cluster "Loyal Customer" cenderung memiliki nilai Frequency dan Monetary yang lebih tinggi dibandingkan dengan cluster lainnya.
                 - Karakteristik masing-masing segmen pelanggan berdasarkan perilaku pembelian mereka dapat diidentifikasi, yang dapat membantu dalam merancang strategi pemasaran yang lebih efektif dan personalisasi penawaran kepada pelanggan.
             ''')
+            
+elif st.session_state.page == "recommendation_system":
+    items, similarity, customer_dict = recommendation_system(df)
+    
+    st.title("Recommendation System")
+    
+    with st.container():
+        st.dataframe(items)
+
+    tab_description, tab_customer = st.tabs(["Recommend Item Description", "Recommend for Single Customer"])
+    
+    with tab_description:
+        col1, col2, col3 = st.columns([1, 3, 1])
+        
+        with col2:
+            st.subheader("Recommend Item Description")
+            item_name_full = st.selectbox("Select, search, or randomize item Description", options=items['Description'])
+            left, right = st.columns(2)
+            
+            with left:
+                if st.button("Recommend Item", key="recommend_item", width='stretch'):
+                    st.session_state.item_name_full = item_name_full
+                    st.session_state.item_name_text = recommend_item_description(item_name_full=st.session_state.item_name_full, top_n=5)
+                    
+                    st.write_stream((stream_data(st.session_state.item_name_text)))
+                    st.rerun()
+                    
+                if 'item_name_full' in st.session_state:
+                    st.write(st.session_state.item_name_text)
+                    
+            with right:
+                if st.button("Randomize Item", key="randomize_item", width='stretch'):
+                    random_item = np.random.choice(items['Description'])
+                    st.session_state.random_item = random_item
+                    st.session_state.random_item_text = recommend_item_description(item_name_full=st.session_state.random_item, top_n=5)
+                    
+                    st.write_stream((stream_data(st.session_state.random_item_text)))
+                    st.rerun()
+                    
+                if 'random_item' in st.session_state:
+                    st.write(st.session_state.random_item_text)
+            
+    with tab_customer:
+        col1, col2, col3 = st.columns([1, 3, 1])
+        
+        with col2:
+            st.subheader("Recommend for Single Customer")
+            id_customer = st.selectbox("Select, search, or randomize CustomerID", options=list(customer_dict.keys()))
+            left, right = st.columns(2)
+            
+            with left:
+                if st.button("Recommend for Customer", key="recommend_customer", width='stretch'):
+                    st.session_state.id_customer = id_customer
+                    st.session_state.id_customer_text = recommend_for_single_customer(id_customer=st.session_state.id_customer, top_n=5)
+                    
+                    st.write_stream(stream_data(st.session_state.id_customer_text))
+                    st.rerun()
+                    
+                if 'id_customer' in st.session_state:
+                    st.write(st.session_state.id_customer_text)
+            
+            with right:
+                if st.button("Randomize CustomerID", key="randomize_customer", width='stretch'):
+                    random_id = np.random.choice(list(customer_dict.keys()))
+                    st.session_state.random_id = random_id
+                    st.session_state.random_id_text = recommend_for_single_customer(id_customer=st.session_state.random_id, top_n=5)
+                    
+                    st.write_stream(stream_data(st.session_state.random_id_text))
+                    st.rerun()
+                    
+                if 'random_id' in st.session_state:
+                    st.write(st.session_state.random_id_text)
